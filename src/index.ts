bda01cd0628808b4430c22e8505e1ae6c57fe8cd
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2023-12-20 19:42:01 +0530

    Add dependencies for cookie-parser and cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import "dotenv/config";
 import connectToDatabase from "./db/mongoose.js";
+import { app } from "./app.js";
 
-connectToDatabase();
+const port: number = parseInt(process.env.PORT!) || 8000;
+connectToDatabase()
+  .then(() => {
+    app.listen(port),
+      () => {
+        console.log(`Listening on port: ${port}`);
+      };
+  })
+  .catch((err) => {
+    console.log(err);
+  });
