79d4159201fb9bb35101ff8f93ab255606c1f621
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2023-12-20 18:26:21 +0530

    Update constants and tsconfig, add database connection logic

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
-console.log("Hello");
-console.log("Mriganka");
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+
+let isConnected: boolean = false;
+
+export const connectToDatabase = async () => {
+  mongoose.set("strictQuery", true);
+
+  //   if MONGODB_URL is not set, then throw an error
+  if (!process.env.MONGODB_URI) {
+    return console.log("MONGODB_URL not found");
+  }
+
+  if (isConnected) {
+    return console.log("MONGODB is already connected");
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: DB_NAME,
+    });
+
+    isConnected = true;
+    // console.log("MongoDB connected");
+  } catch (error) {
+    console.log("MongoDB not connected");
+  }
+};
